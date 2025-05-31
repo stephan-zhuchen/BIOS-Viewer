8090d6fae82c33efb1c1ac4a7f510e2f09f3c863
Zhu Chen <stephan.zhuchen@gmail.com>
2025-05-31 09:57:41 +0000

    change cli tool name to binary-tool
    Signed-off-by: Zhu Chen <stephan.zhuchen@gmail.com>

--- Cli/main.cpp
+++ Cli/main.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 #include "BaseLib.h"
 #include "cliview.h"
+#include <string>
 // #include "vld.h"
+ 
+void help()
+{
+    std::cout << "Usage: binary-tool <filename>" << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
     CliView view;
-    if (argc == 2){
+    if (argc == 2) {
+        if (std::string(argv[1]) == "--help" || std::string(argv[1]) == "-h") {
+            help();
+            return 0;
+        }
+        if (std::string(argv[1]) == "--version" || std::string(argv[1]) == "-v") {
+            std::cout << "Binary Tool Version 1.16" << std::endl;
+            return 0;
+        }
         view.openFile(argv[1]);
+    } else {
+        help();
+        return 1;
     }
     return 0;
 }
-
